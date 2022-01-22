ca54444e8080c8af97314f4c7fa3bc40071c2469
MishaZimuk <mishazimuk@gmail.com>
2022-01-22 14:20:41 +0200

    homeWork13

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let str = '';
 //Task1
 function myForEach(array, callbackFunction) {
     for (let i = 0; i < ar.length; i++) {
-        callbackFunction(array[i]); 
+        callbackFunction(array[i], i, array); 
     }
 }
 myForEach(ar, n => str += '#' + n);
@@ -12,7 +12,7 @@ console.log(str);
 const ar1 = [-10, 50, -13, 80, 40, 70];
 function myMap( array, callBackFunc) {
     const arr1 = [];
-    myForEach(array, n => arr1.push(callBackFunc(n)));
+    myForEach(array, (n, i, ar) => arr1.push(callBackFunc(n, i, ar)));
     return arr1;
 }
 const ar2 = myMap(ar1, n => n = n*2)
