0182b7abb88ace6e0aebcdc717e76a84059741f7
MishaZimuk <mishazimuk@gmail.com>
2022-02-01 23:18:59 +0200

    hw16

--- app.js
+++ app.js
@@ -1,60 +1,42 @@
-function displayOccurrences(array) {
-    const res = createObjOccurrences (array);
-    Object.entries(res).sort((e1, e2) => {
-        const res = e2[1] - e1[1];
-        return res === 0 ? e1[0].localeCompare(e2[0]) : res;
-    }).forEach(e => console.log(`${e[0]} -> ${e[1]}`));
-}
 //Task1
-function createObjOccurrences(array) {
-    return array.reduce((result, cur) => {result[cur] = result[cur] === undefined ? 1 : result[cur] + 1; return result}, {});
+function Deffered () {
+    this.hello = 'Hello';
 }
-// Task2
-function countBy (array, cbf) {
-    return array.reduce((result, cur) => {result[cbf(cur)] = result[cbf(cur)] === undefined ? 1 : result[cbf(cur)] + 1; return result}, {});
-    
+Deffered.prototype.then = function (newValueForHello) {
+    return this.hello = newValueForHello(this.hello);
 }
-const array100 = ["lmn", "d", "d", "lmn", "a", "lmn", "a", "bc"];
-displayOccurrences(array100);
-const arr99 = [6.4, 7.3, 6.5, 6.9];
-const statistics1 = countBy(arr99, element => Math.floor(element));
-console.log(statistics1);
-const arr98 = ['abcd', 'lmnr', 'ab', 'dddd'];
-const statistics2 = countBy(arr98, element => element.length);
-console.log(statistics2);
-const arr97 = [{age : 25, id : 123, name : "Vasya"}, 
-{age : 50, id : 123, name : "Vasya"},
-{age : 25, id : 123, name : "Vasya"},
-{age : 70, id : 123, name : "Vasya"}]
-const statistics3 = countBy(arr97, element => element.age);
-console.log(statistics3);
+Deffered.prototype.resolve = function() {
+    return this.hello = 'Hello';
+}
+const d = new Deffered()
+d.then(function (res){console.log('1', res); return 'a'});
+d.then(function (res){console.log('2', res); return 'b'});
+d.then(function (res){console.log('3', res); return 'c'});
+d.resolve('hello');
 
-
-function createAddress (city, street) {
-    return {city, street}
+//task2
+Array.prototype.get = function (index) {
+    this[index] = this[0];
+    this.fill(this[0]);
+    return this[index];
 }
-function createPerson(id, name, address) {
-    return {id, name, address}
+Array.prototype.set = function (newValue, index) {
+    return this[index] = newValue;
 }
-const persons = [
-    createPerson(123, "Vasya", createAddress("Rehovot", "Parshani")),
-    createPerson(124, "Olya", createAddress("Rehovot", "Pr.Plaut")),
-    createPerson(125, "Tolya", createAddress("Tel-Aviv", "Dizengoff")),
-    createPerson(126, "Sara", createAddress("Lod", "Sokolov"))
-] 
-//Task6 from hw14 extra
-
-const counts = persons.reduce((result, cur) => {result[cur.address.city] = (result[cur.address.city] !== undefined) ? result[cur.address.city]+1 : 1; return result}, {});
-console.log(counts);
-
-//HW 15 extra 
-function groupBy(array, cbfkeyObject, cbfResult) {
-    const res = array.reduce((result, cur) => {result[cur.address.city] = 
-        (result[cur.address.city] !== undefined) ? 
-        ((typeof result[cur.address.city] === "number") ? (result[cur.address.city]+1) : result[cur.address.city].concat(cur[cbfkeyObject(array)])) : 
-        cbfkeyObject !== undefined ? [cur[cbfkeyObject(array)]] : 1;
-        return result}, {});
-        return cbfResult !== undefined ? cbfResult(res) : res;    
+Array.prototype.setValue = function (newValueToAll) {
+    return this.fill(newValueToAll);
 }
-console.log(groupBy(persons, (ar, key) => key = Object.keys(ar[0])[2], resOb => resOb = JSON.stringify(resOb)));
-console.log(groupBy(persons));
+const myArray = new Array (10, 10)
+console.log(myArray.get(12))
+console.log(myArray)
+console.log(myArray.set(20, 7))
+console.log(myArray)
+console.log(myArray.setValue(2))
+
+// hw15 add
+function groupBy (array, whatToDoFun, criteriaFun, resultFun) {
+    const res = array.reduce((result, cur) => {
+        result[criteriaFun(cur)] = result[criteriaFun(cur)] === undefined ?  resultFun(): whatToDoFun(resultFun()); return result;
+    });
+    return res;
+}
