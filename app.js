c0a84ec8a70d6b52b7a983b22fde084e816fa939
MishaZimuk <mishazimuk@gmail.com>
2022-02-04 05:29:08 +0200

    HW17

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
-//Task1
-function Deffered () {
-    this.hello = 'Hello';
+class Person {
+    #id;
+    #name;
+    constructor(id, name) {
+        this.#id = id;
+        this.#name = name;
+    }
+    getId() {
+        return this.#id;
+    }
+    getName() {
+        return this.#name;
+    }
+    toString() {
+        return `id: ${this.#id} name: ${this.#name} `;
+    }
 }
-Deffered.prototype.then = function (newValueForHello) {
-    return this.hello = newValueForHello(this.hello);
+class Employee extends Person {
+    #salary;
+    constructor(id, name, salary) {
+        super(id, name);
+        this.#salary = salary;
+    } 
+    computeSalary() {
+        return this.#salary;
+    }
+    toString() {
+        return (super.toString() + `salary : ${this.computeSalary()}`)
+    }
 }
-Deffered.prototype.resolve = function() {
-    return this.hello = 'Hello';
+class Child extends Person {
+    #kindergarten;
+    constructor(id, name, kindergarten) {
+        super(id, name);
+        this.#kindergarten = kindergarten;
+    }
+    getKindergarten() {
+        return this.#kindergarten;
+    }
+    toString() {
+        return `${super.toString()} kindergarten: ${this.#kindergarten}`
+    }
 }
-const d = new Deffered()
-d.then(function (res){console.log('1', res); return 'a'});
-d.then(function (res){console.log('2', res); return 'b'});
-d.then(function (res){console.log('3', res); return 'c'});
-d.resolve('hello');
-
+class WageEmployee extends Employee {
+    #hours
+    #wage
+    constructor(id, name, salary, hours, wage) {
+        super(id, name, salary);
+        this.#hours = hours;
+        this.#wage = wage;
+    }
+    computeSalary() { 
+        return super.computeSalary() + this.#hours * this.#wage
+    } 
+}
+//hw 17
+const person = [
+    new Child(100, 'Olya', 'Shalom'),
+    new Child(101, 'Serega', 'Boker'),
+    new Child(102, 'Kolya', 'Shalom'),
+    new Employee(103, 'Vasya', 1000),
+    new WageEmployee(104, 'Tolya', 1000, 10, 100)
+]
+//task1
+function countOfPersonType(persons, type) {
+    return persons.filter(n => n.constructor.name === type).length;
+}
+console.log(countOfPersonType(person, "WageEmployee"));
 //task2
-Array.prototype.get = function (index) {
-    this[index] = this[0];
-    this.fill(this[0]);
-    return this[index];
+function computeSalaryBudget(persons) {
+    return persons.filter(n => n.constructor.name !== "Child").reduce((sum, per) => sum + per.computeSalary(), 0);
 }
-Array.prototype.set = function (newValue, index) {
-    return this[index] = newValue;
+console.log(computeSalaryBudget(person));
+//task3
+function countChildrenKindergarten (persons, kindergarten) {
+   return persons.reduce((res, per) => {per.constructor.name === "Child" 
+   && per.getKindergarten() === kindergarten 
+   && res++; 
+   return res}, 0)
 }
-Array.prototype.setValue = function (newValueToAll) {
-    return this.fill(newValueToAll);
-}
-const myArray = new Array (10, 10)
-console.log(myArray.get(12))
-console.log(myArray)
-console.log(myArray.set(20, 7))
-console.log(myArray)
-console.log(myArray.setValue(2))
-
-// hw15 add
-function groupBy (array, whatToDoFun, criteriaFun, resultFun) {
-    const res = array.reduce((result, cur) => {
-        result[criteriaFun(cur)] = result[criteriaFun(cur)] === undefined ?  resultFun(): whatToDoFun(resultFun()); return result;
-    });
-    return res;
-}
+console.log(countChildrenKindergarten (person, "Shalom"));
