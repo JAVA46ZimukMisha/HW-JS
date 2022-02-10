539bc1bcf7adbac6f9ba2d8ff2d76aaa4e51650a
MishaZimuk <mishazimuk@gmail.com>
2022-02-10 21:00:14 +0200

    mybind

--- app.js
+++ app.js
@@ -1,92 +1,32 @@
-class Person {
-    #id;
-    #name;
-    constructor(id, name) {
-        this.#id = id;
-        this.#name = name;
-    }
-    getId() {
-        return this.#id;
-    }
-    getName() {
-        return this.#name;
-    }
-    toString() {
-        return `id: ${this.#id} name: ${this.#name} `;
-    }
-}
-class Employee extends Person {
-    #salary;
-    constructor(id, name, salary) {
-        super(id, name);
-        this.#salary = salary;
-    } 
-    computeSalary() {
-        return this.#salary;
-    }
-    toString() {
-        return (super.toString() + `salary : ${this.computeSalary()}`)
-    }
-}
-class Child extends Person {
-    #kindergarten;
-    constructor(id, name, kindergarten) {
-        super(id, name);
-        this.#kindergarten = kindergarten;
-    }
-    getKindergarten() {
-        return this.#kindergarten;
-    }
-    toString() {
-        return `${super.toString()} kindergarten: ${this.#kindergarten}`
-    }
-}
-class WageEmployee extends Employee {
-    #hours
-    #wage
-    constructor(id, name, salary, hours, wage) {
-        super(id, name, salary);
-        this.#hours = hours;
-        this.#wage = wage;
-    }
-    computeSalary() { 
-        return super.computeSalary() + this.#hours * this.#wage
-    } 
-}
-//hw 17
-const person = [
-    new Child(100, 'Olya', 'Shalom'),
-    new Child(101, 'Serega', 'Boker'),
-    new Child(102, 'Kolya', 'Shalom'),
-    new Employee(103, 'Vasya', 1000),
-    new WageEmployee(104, 'Tolya', 1000, 10, 100)
-]
-//task1
-function countOfPersonType(persons, type) {
-    return persons.filter(n => n.constructor.name === type).length;
-}
-console.log(countOfPersonType(person, "WageEmployee"));
-//task2
-function computeSalaryBudget(persons) {
-    const allEmployees = persons.filter(p => !!p.computeSalary);
-    const salaryValues = allEmployees.map(p => p.computeSalary());
-    return salaryValues.reduce((res,cur) => res+cur);
-}
-console.log(computeSalaryBudget(person));
-//task3
-function countChildrenKindergarten (persons, kindergarten) {
-   return persons.reduce((res, per) => {per.constructor.name === "Child" 
-   && per.getKindergarten() === kindergarten 
-   && res++; 
-   return res}, 0)
-}
-console.log(countChildrenKindergarten (person, "Shalom"));
+// const point = {
+//     x: 3,
+//     y: 4
+// };
+// function displayPointInSpace(z, t) {
+//     //"this" - reference to any object having properties x and y
+//     console.log(`x: ${this.x}, y: ${this.y}, z: ${z}, t: ${t}`);
+// }
+// // displayPointInSpace.call(point, 10, 20);
+// // displayPointInSpace.mybind(point, 10, 20)();
+// // displayPointInSpace.apply(point, [10, 20]);
+// Function.prototype.mybind = function(thisObj, ...args) {
+//     //this - reference to any functional object (in example - displayPointInSpace)
+//     //thisObj - reference to any object (in example - point)
+     
+//     return (...params) => {
+//         thisObj.method123456 = this;
+//         const res = thisObj.method123456(...args.concat(params));
+//         delete thisObj.method123456;
+//         return res;
+//     }
+// }
+// // arguments are passed at function call
+// const funDisplay = displayPointInSpace.mybind(point);
+// funDisplay(10, 20);
+// //all arguments are bound by the method "mybind"
+// const funDisplayArguments = displayPointInSpace.mybind(point, 10, 20);
+// funDisplayArguments();
+// //mixed - part of arguments are bound by the method "mybind" and other part of arguments are passed at function call
+// const funDisplayMixed = displayPointInSpace.mybind(point, 10);
+// funDisplayMixed(20);
 
-function testOutput(fun, expected) {
-    console.log(`function ${fun.name}; expected result: ${expected}; actual result: ${fun()}`)
-}
-testOutput(WageEmployee.prototype.computeSalary.bind(person[4]), 2000);
-testOutput(computeSalaryBudget.bind(undefined, person), 3000);
-testOutput(countChildrenKindergarten.bind(undefined, person, "Shalom"), 2)
-
-testOutput(person[4].computeSalary(), 2000);
